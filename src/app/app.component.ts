4cd0a8f429a28fc727bfbd75e2b48cac845e58a3
pragya <pragya.sumani28@gmail.com>
2021-08-08 11:50:27 +0530

    intial features-add,delete and search notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { CompileShallowModuleMetadata, NullTemplateVisitor } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { NoteComponent } from './note/note.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'save-notes';
+  expandTextArea = false;
+  color="red";
+  arr : NoteComponent[] = [];
+  obj : NoteComponent = new NoteComponent();
+  ctitle : string = "";
+  ctext : string = "";
+  keyword = "";
+  id = -1;
+  tempArr : NoteComponent[] = [];
+
+
+  expand(){
+    this.expandTextArea = true;
+    console.log("expand");
+  }
+  collapse(){
+    this.expandTextArea = false;
+    console.log("collapse");
+  }
+
+  createNote(){
+  
+    console.log(this.ctitle);
+    console.log(this.ctext); 
+    this.obj = new NoteComponent();
+    this.id = this.id + 1;
+    this.obj.createNote(this.ctitle, this.ctext, this.id);
+    this.ctitle = "";
+    this.ctext = "";
+    this.arr.push(this.obj);
+
+    console.log(this.arr.length);
+    //console.log("from create method "+this.arr);
+  }
+
+  submitValues(value : any){
+    console.log(value);
+  }
+
+  deleteNote(arg : any){
+    console.log(arg.id);
+    this.arr.forEach((value, index)=>{
+      if(value.id==arg.id)
+        this.arr.splice(index,1);
+    });
+  }
+
+  search(keywordToMatch : string){
+    this.keyword = keywordToMatch;
+    console.log(this.keyword);
+    for(let x=0; x<this.arr.length; x++){
+      if(this.arr[x].title===this.keyword){
+        this.tempArr.push(this.arr[x]);
+      }
+    }
+    console.log(this.tempArr);
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
